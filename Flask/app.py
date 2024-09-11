f732e99777b296eaef7aed39567bf2ba13be86f2
Shan <173754623+c3lima61@users.noreply.github.com>
2024-09-12 01:23:21 +0900

    Script has been amended with some testing information, more curl commands as we missed one for updating. Commenting is complete, as well as a minor change in how the delete and update endpoints return errors, no longer 404 renders.

--- Flask/app.py
+++ Flask/app.py
@@ -10,82 +10,92 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db' # Telling the app wh
 db = SQLAlchemy(app)  # Initialize the SQLAlchemy extension for database interaction
 app.app_context().push() # Gives context for database access
 
-""" The Todo model for storing tasks in the database """
+# The model for storing tasks in the database
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    # creates Id for observation
-    def __repr__(self):
-        return '<Task %r>' % self.id
+    id = db.Column(db.Integer, primary_key=True)    # Gives each task a unique id number and sets id column as the primary key
+    content = db.Column(db.String(200), nullable=False) # The content can have a max length of 200 characters and cannot be left empty
+    completed = db.Column(db.Integer, default=0)    # Status of task = 0 for incomplete, 1 for complete
+    date_created = db.Column(db.DateTime, default=datetime.now) # Provides timestamp when the task was created
 
-# Route from server
+    def __repr__(self):
+        return '<Task %r>' % self.id    # Mainly for debugging purposes using the string representation
+
+# This function defines the main route for task creation and task retrieval
 @app.route("/", methods=['POST', 'GET'])
 def task_manager():
     if request.method == 'POST':
-        task_content = request.json['content']  # Get data from the mock mobile app in JSON format
-        new_task = Todo(content=task_content)
+        task_content = request.json['content']  # Get data (task content) from the frontend (must be JSON input)
+        new_task = Todo(content=task_content)   # Create the new task (object)
 
         try:
-            db.session.add(new_task)
-            db.session.commit()
-            return jsonify({'message': 'Successfully added task.'}), 201
+            db.session.add(new_task)    # New task is added to the database session
+            db.session.commit() # Commit the session to ensure changes are saved
+            return jsonify({'message': 'Successfully added task.'}), 201    # Success message and status
         except Exception as e:
-            return jsonify({'error': f'There was an issue adding the task: {e}'}), 500
+            return jsonify({'error': f'There was an issue adding the task: {e}'}), 500  # Error message and status
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        tasks = Todo.query.order_by(Todo.date_created).all()    # Retrieves all tasks by creation date, will be ordered by creation date.
         tasks_list = [{"id": task.id, "content": task.content, "completed": task.completed, "date_created": task.date_created} for task in tasks]
-        return jsonify(tasks_list), 200
+        return jsonify(tasks_list), 200 # Return the list of dictionaries of which each dictionary represents a task from the database
 
+# This function defines the route for task deletion by task id
 @app.route('/delete/<int:id>', methods=['DELETE'])
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = Todo.query.filter_by(id=id).first()    # Look for task by id and if not found return none
+
+    if not task_to_delete:
+        return jsonify({'error': 'Task not found.'}), 404   # Return error
 
     try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return jsonify({'message': 'Successfully deleted task.'}), 200
+        db.session.delete(task_to_delete)   # Delete task from database session
+        db.session.commit() # Commit the session to ensure changes are saved
+        return jsonify({'message': 'Successfully deleted task.'}), 200  # Success message
     except Exception as e:
-        return jsonify({'error': f'Problem occurred when deleting task: {e}'}), 500
+        return jsonify({'error': f'Problem occurred when deleting task: {e}'}), 500 # Error message
 
+# This function defines the route for task updates by task id
 @app.route('/update/<int:id>', methods=['PUT'])
 def update(id):
-    task = Todo.query.get_or_404(id)
-    task.content = request.json['content']  # Get updated content in JSON format
+    task_to_update = Todo.query.filter_by(id=id).first()  # Look for task by id and if not found return none
+
+    if not task_to_update:
+        return jsonify({'error': 'Task not found.'}), 404   # Return error
+
+    task_to_update.content = request.json['content']  # Update the task content with the new data
 
     try:
-        db.session.commit()
-        return jsonify({'message': 'Successfully updated task.'}), 200
+        db.session.commit() # Commit the session to ensure changes are saved
+        return jsonify({'message': 'Successfully updated task.'}), 200  # Success message
     except:
-        return jsonify({'error': 'There was an issue updating your observation'}), 500
+        return jsonify({'error': 'There was an issue updating your observation'}), 500  # Error message
 
+# This function defines the route for exporting tasks as a CSV file
 @app.route('/export', methods=['GET'])
 def export():
     try:
-        # Fetching tasks from the database
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        # Fetching tasks from the database (ordered by creation date)
+        tasks_to_export = Todo.query.order_by(Todo.date_created).all()
 
-        if not tasks:
-            return jsonify({'error': 'No tasks available to export'}), 404
+        if not tasks_to_export:
+            return jsonify({'error': 'No tasks available to export'}), 404  # Return error
 
-        # In-memory CSV creation
+        # In-memory CSV file creation ---FOR TESTING ONLY---TEMP DATA---NO ACTUAL FILE---
         output = StringIO()
-        writer = csv.writer(output)
-        writer.writerow(['ID', 'Content', 'Completed', 'Date Created'])
+        writer = csv.writer(output) # CSV writer object
+        writer.writerow(['ID', 'Content', 'Completed', 'Date Created']) # Write CSV header
 
-        # Writing task data into CSV structure
-        for task in tasks:
+        # Write each task's data as a row in the CSV file
+        for task in tasks_to_export:
             writer.writerow([task.id, task.content, task.completed, task.date_created])
 
-        output.seek(0)
+        output.seek(0) # Move back to the beginning of the file to return it as a response
 
-        # Returning CSV response
+        # Return the CSV as a downloadable file with the correct headers
         return Response(output, mimetype="text/csv", headers={"Content-Disposition": "attachment;filename=tasks.csv"})
 
     except Exception as e:
-        return jsonify({'error': f'An error occurred while exporting the CSV: {e}'}), 500
+        return jsonify({'error': f'An error occurred while exporting the CSV: {e}'}), 500   # Error message
 
-# Setting local host to 50100, use localhost:50100
+# Setting local host to 50100, ensure debug mode is turned False when testing is finished
 if __name__ == '__main__':
     app.run(port=50100, debug=True)
--- Flask/curl_script.sh
+++ Flask/curl_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Task will be completed with 0 even though you return a success message.
+# The task is created with the default value, as defined in the Todo class.
+
 # POST request (add a new task)
 curl -X POST http://localhost:50100/ -H "Content-Type: application/json" -d '{"content": "Test task"}'
 curl -X POST http://localhost:50100/ -H "Content-Type: application/json" -d '{"content": "CSV task"}'
@@ -9,6 +12,12 @@ curl -X GET http://localhost:50100/
 
 # DELETE request (remove a task with ID 1)
 curl -X DELETE http://localhost:50100/delete/1
+
+# PUT request to update a task (task with ID 2)
+curl -X PUT http://localhost:50100/update/2 -H "Content-Type: application/json" -d '{"content": "updated CSV task"}'
+
+# Confirm update with a GET request
+curl -X GET http://localhost:50100/
 
 # EXPORT THE CSV (not formatted)
 curl -X GET http://localhost:50100/export -o exported_tasks.csv
